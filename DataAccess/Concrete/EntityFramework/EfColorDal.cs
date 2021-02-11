1f13bd435d818b2f948741f02c756b43a8def79d
oguzhan-derin <oguzhanderinn@gmail.com>
2021-02-12 02:18:52 +0300

    New Details

--- DataAccess/Concrete/EntityFramework/EfColorDal.cs
+++ DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,26 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfColorDal :EfEntityRepositoryBase<Color,ReCapProjectDbContext>, IColorDal
+    public class EfColorDal : EfEntityRepositoryBase<Color, ReCapProjectDbContext>, IColorDal
 
     {
-       
+        public List<CarDetailDto> GetColorDetail()
+        {
+            using (ReCapProjectDbContext context = new ReCapProjectDbContext())
+            {
+                var result = from ca in context.Cars
+                             join b in context.Brands
+                             on ca.Id equals b.BrandId
+                             join co in context.Colors
+                             on ca.ColorId equals co.ColorId
+                             select new CarDetailDto
+                             {
+                                 Id = ca.Id,
+                                 BrandName = b.BrandName,
+                                 ColorName = co.ColorName
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
